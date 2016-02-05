648dfbdfa0846a17cd938830d8f9eee64d38bb5e
Peter <peter.zsoldos@gmail.com>
2016-02-05 11:35:07 +0100

    initial commit - app skeleton

---
+++ README.rst
@@ -0,0 +1,22 @@
+=============================
+djactasauth
+=============================
+
+Django authentication that allows you to login as someone else
+
+Quickstart
+----------
+
+Install djactasauth::
+
+    pip install django-act-as-auth
+
+Then use it in a project::
+
+    import djactasauth
+
+Release Notes
+-------------
+
+* 0.1.0 - initial release
+  * supports Django 1.5 and 1.8 on python 2.7
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import djactasauth
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = djactasauth.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('make release')
+    sys.exit()
+
+readme = open('README.rst').read()
+description = 'Django authentication allowing admins to login as another user'
+
+setup(
+    name='djactasauth',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Paessler AG',
+    url='https://github.com/PaesslerAG/django-act-as-auth',
+    packages=[
+        'djactasauth',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django-act-as-auth',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
