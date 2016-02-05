e0a2a1b4a8733ecd8856626d8329b97870f467c8
Peter <peter.zsoldos@gmail.com>
2016-02-05 16:45:19 +0100

    refactor - extract helper method from class

--- tests/testapp/tests.py
+++ tests/testapp/tests.py
@@ -8,6 +8,14 @@ from django.test.utils import override_settings
 from djactasauth.backends import FilteredModelBackend, ActAsModelBackend, OnlySuperUserCanActAsModelBackend
 
 
+def create_user(username, password='password', is_superuser=False, is_staff=False):
+    user = User(username=username, is_superuser=is_superuser)
+    user.set_password(password)
+    user.save()
+    return user
+
+
+
 class FilteredBackendTestCase(TransactionTestCase):
 
     def test_it_is_a_model_backend(self):
@@ -16,9 +24,9 @@ class FilteredBackendTestCase(TransactionTestCase):
             FilteredModelBackend.__mro__)
 
     def test_can_declare_filters_which_apply_to_get_user(self):
-        staff = User.objects.create(username='staff', is_staff=True, is_superuser=False)
-        superuser = User.objects.create(username='superuser', is_staff=True, is_superuser=True)
-        customer = User.objects.create(username='customer', is_staff=False, is_superuser=False)
+        staff = create_user(username='staff', is_staff=True, is_superuser=False)
+        superuser = create_user(username='superuser', is_staff=True, is_superuser=True)
+        customer = create_user(username='customer', is_staff=False, is_superuser=False)
         for u in [staff, superuser, customer]:
             u.set_password('password')
             u.save()
@@ -59,17 +67,17 @@ class ActAsModelBackendTestCase(TransactionTestCase):
             ActAsModelBackend.__mro__)
 
     def test_can_authenticate_user(self):
-        user = self.create_user(username='user', password='password')
+        user = create_user(username='user', password='password')
         self.assertEqual(user, self.authenticate(username='user', password='password'))
 
     def test_can_become_another_user_with_own_password(self):
-        admin = self.create_user(username='admin', password='admin password')
-        user = self.create_user(username='user', password='user password')
+        admin = create_user(username='admin', password='admin password')
+        user = create_user(username='user', password='user password')
         self.assertEqual(None, self.authenticate(username='admin/user', password='user password'))
         self.assertEqual(user, self.authenticate(username='admin/user', password='admin password'))
 
     def test_cannot_become_nonexistent_user(self):
-        admin = self.create_user(username='admin', password='password')
+        admin = create_user(username='admin', password='password')
         self.assertEqual(None, self.authenticate(username='admin/user', password='password'))
 
     def test_authenticate_does_not_fire_login_signal(self):
@@ -77,8 +85,8 @@ class ActAsModelBackendTestCase(TransactionTestCase):
             self.fail(
                 'should not have fired login signal but did for %r' % user)
 
-        admin = self.create_user(username='admin', password='admin password')
-        user = self.create_user(username='user', password='user password')
+        admin = create_user(username='admin', password='admin password')
+        user = create_user(username='user', password='user password')
 
         auth_signals.user_logged_in.connect(should_not_fire_login_signal)
         try:
@@ -89,9 +97,9 @@ class ActAsModelBackendTestCase(TransactionTestCase):
 
     def test_regression_test_for_only_super_user_can_act_as_model_backend(self):
 
-        admin1 = self.create_user(username='admin1', password='admin1 password', is_superuser=True)
-        admin2 = self.create_user(username='admin2', password='admin2 password', is_superuser=True)
-        user = self.create_user(username='user', password='user password', is_superuser=False)
+        admin1 = create_user(username='admin1', password='admin1 password', is_superuser=True)
+        admin2 = create_user(username='admin2', password='admin2 password', is_superuser=True)
+        user = create_user(username='user', password='user password', is_superuser=False)
 
         self.assertEqual(None, self.authenticate(username='user/admin1', password='user password', backend_cls=OnlySuperUserCanActAsModelBackend))
         self.assertEqual(None, self.authenticate(username='user/admin2', password='user password', backend_cls=OnlySuperUserCanActAsModelBackend))
@@ -103,8 +111,8 @@ class ActAsModelBackendTestCase(TransactionTestCase):
         self.assertEqual(None, self.authenticate(username='admin2/admin1', password='admin2 password', backend_cls=OnlySuperUserCanActAsModelBackend))
 
     def test_can_customize_can_act_as_policy_by_subclassing(self):
-        alice = self.create_user(username='alice', password='alice')
-        bob = self.create_user(username='bob', password='bob')
+        alice = create_user(username='alice', password='alice')
+        bob = create_user(username='bob', password='bob')
 
         class OnlyShortUserNamesCanActAs(ActAsModelBackend):
 
@@ -115,12 +123,6 @@ class ActAsModelBackendTestCase(TransactionTestCase):
         self.assertEqual(alice, self.authenticate(username='bob/alice', password='bob', backend_cls=OnlyShortUserNamesCanActAs))
 
 ###
-
-    def create_user(self, username, password, is_superuser=False):
-        user = User(username=username, is_superuser=is_superuser)
-        user.set_password(password)
-        user.save()
-        return user
 
     def authenticate(self, username, password, backend_cls=None):
         if not backend_cls:
