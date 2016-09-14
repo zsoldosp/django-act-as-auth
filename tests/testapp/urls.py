6c6a38e0c1f7b455618fefa3fbf42e033a61827d
Peter <peter.zsoldos@gmail.com>
2016-09-14 10:59:58 +0200

    django.conf.urls.patterns have been removed in 1.10

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from djactasauth.views import act_as_login_view
 from testapp.views import whoami
 
 
-urlpatterns = patterns(
-    'testapp',
+urls = [
     url(r'^login/$', act_as_login_view, {}, 'login'),
     url(r'^whoami/$', whoami),
-)
+]
+try:
+    from django.conf.urls import patterns
+    urlpatterns = patterns('testapp', *urls)
+except ImportError:
+    urlpatterns = urls
