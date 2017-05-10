6ffb69a06d37415a127a0cfe2ebfba7c7fc680c7
Peter <peter.zsoldos@gmail.com>
2017-05-10 14:12:51 +0200

    get rid of removed in django 1.10 warnings

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
 from djactasauth.views import act_as_login_view
 from testapp.views import whoami
+import django
 
 
 urls = [
     url(r'^login/$', act_as_login_view, {}, 'login'),
     url(r'^whoami/$', whoami),
 ]
-try:
+if django.VERSION[:2] < (1, 8):
     from django.conf.urls import patterns
     urlpatterns = patterns('testapp', *urls)
-except ImportError:
+else:
     urlpatterns = urls
