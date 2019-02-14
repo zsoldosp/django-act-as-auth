234cda351d0bc022311801b5b40fc434da4536e5
Sebastian Bonhag <sebastian.bonhag@paessler.com>
2019-02-14 12:41:50 +0100

    Remove compatibility code for old Django versions.

--- djactasauth/views.py
+++ djactasauth/views.py
@@ -1,4 +1,3 @@
-import django
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.debug import sensitive_post_parameters
 from djactasauth.forms import InitialValuesFromRequestGetFormMixin
@@ -6,14 +5,6 @@ from djactasauth.forms import InitialValuesFromRequestGetFormMixin
 
 def get_login_form(request, authentication_form=AuthenticationForm, **kwargs):
     base_form = authentication_form
-    if django.VERSION[:2] < (1, 6):
-        class CaptureRequestActAsAuthForm(base_form):
-            def __init__(self, *a, **kw):
-                if request.method == 'POST':
-                    kw.setdefault('request', request)  # "backport" from 1.6
-                super(CaptureRequestActAsAuthForm, self).__init__(*a, **kw)
-
-        base_form = CaptureRequestActAsAuthForm
 
     if not issubclass(base_form, InitialValuesFromRequestGetFormMixin):
         class InitialFromQueryAuthForm(
@@ -25,9 +16,6 @@ def get_login_form(request, authentication_form=AuthenticationForm, **kwargs):
                         ('username',)
 
         base_form = InitialFromQueryAuthForm
-    else:
-        # TODO: raise warning if it doesn't have username in query2initial
-        pass
 
     return base_form
 
